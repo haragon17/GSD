5b24ca5827ea342037c325079c36e51e31323328
Jakob <gsd@192.168.3.189>
2016-07-27 10:26:43 +0700

    Audit logging/History state for Customer table

--- src/com/gsd/dao/impl/CustomerDaoImpl.java
+++ src/com/gsd/dao/impl/CustomerDaoImpl.java
@@ -8,6 +8,9 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.gsd.dao.CustomerDao;
 import com.gsd.model.Customer;
+import com.gsd.model.KeyAccountManager;
+import com.gsd.security.UserDetailsApp;
+import com.gsd.security.UserLoginDetail;
 
 public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
 
@@ -43,6 +46,14 @@ public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
 
 	@Override
 	public void updateCustomer(Customer cus) {
+		
+		Customer cus_audit = new Customer();
+		cus_audit = getJdbcTemplate().queryForObject("select * from customer where cus_id ="+cus.getCus_id(), new BeanPropertyRowMapper<Customer>(Customer.class));
+		KeyAccountManager key_acc_audit = new KeyAccountManager();
+		key_acc_audit = getJdbcTemplate().queryForObject("select * from key_account_mng where key_acc_id ="+cus_audit.getKey_acc_id(), new BeanPropertyRowMapper<KeyAccountManager>(KeyAccountManager.class));
+		KeyAccountManager key_acc = new KeyAccountManager();
+		key_acc = getJdbcTemplate().queryForObject("select * from key_account_mng where key_acc_id ="+cus.getKey_acc_id(), new BeanPropertyRowMapper<KeyAccountManager>(KeyAccountManager.class));
+		
 		String sql = "update customer set cus_name=?, "
 				+ "cus_code=?, "
 				+ "key_acc_id=?, "
@@ -70,6 +81,148 @@ public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
 			cus.getCus_id()
 		});
 		
+		UserDetailsApp user = UserLoginDetail.getUser();
+		
+		if(!cus_audit.getTransfer_dtl().equals(cus.getTransfer_dtl())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Transfer Detail",
+					cus_audit.getTransfer_dtl(),
+					cus.getTransfer_dtl(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getPayment().equals(cus.getPayment())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Payment",
+					cus_audit.getPayment(),
+					cus.getPayment(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getBill_to().equals(cus.getBill_to())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Bill to",
+					cus_audit.getBill_to(),
+					cus.getBill_to(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getCus_phone().equals(cus.getCus_phone())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Phone",
+					cus_audit.getCus_phone(),
+					cus.getCus_phone(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getCus_email().equals(cus.getCus_email())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"E-Mail",
+					cus_audit.getCus_email(),
+					cus.getCus_email(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getContact_person().equals(cus.getContact_person())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Contact Person",
+					cus_audit.getContact_person(),
+					cus.getContact_person(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getAddress().equals(cus.getAddress())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Address",
+					cus_audit.getAddress(),
+					cus.getAddress(),
+					"Updated"
+			});
+		}
+		
+		if(cus_audit.getKey_acc_id() != cus.getKey_acc_id()){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Key Account Manager",
+					key_acc_audit.getKey_acc_name(),
+					key_acc.getKey_acc_name(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getCus_code().equals(cus.getCus_code())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Customer Code",
+					cus_audit.getCus_code(),
+					cus.getCus_code(),
+					"Updated"
+			});
+		}
+		
+		if(!cus_audit.getCus_name().equals(cus.getCus_name())){
+			String cus_name = "INSERT INTO audit_logging VALUES (?,?,?,?,now(),?,?,?,?)";
+			this.getJdbcTemplate().update(cus_name, new Object[]{
+					getLastAuditId(),
+					cus.getCus_id(),
+					"Customer",
+					user.getUserModel().getUsr_name(),
+					"Customer Name",
+					cus_audit.getCus_name(),
+					cus.getCus_name(),
+					"Updated"
+			});
+		}
+		
 	}
 
 	@Override
@@ -77,6 +230,14 @@ public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
 		
 		String sql = "SELECT max(cus_id) from customer";
 		 
+		int id = getJdbcTemplate().queryForInt(sql);
+		return id+1;
+	}
+	
+	public int getLastAuditId() {
+		
+		String sql = "SELECT max(aud_id) from audit_logging";
+		
 		int id = getJdbcTemplate().queryForInt(sql);
 		return id+1;
 	}
@@ -101,14 +262,43 @@ public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
 			cus.getTransfer_dtl()
 		});
 		
+		UserDetailsApp user = UserLoginDetail.getUser();
+		
+		KeyAccountManager key_acc = new KeyAccountManager();
+		key_acc = getJdbcTemplate().queryForObject("select * from key_account_mng where key_acc_id ="+cus.getKey_acc_id(), new BeanPropertyRowMapper<KeyAccountManager>(KeyAccountManager.class));
+		
+		String audit = "INSERT INTO audit_logging (aud_id,parent_id,parent_object,commit_by,commit_date,commit_desc) VALUES (?,?,?,?,now(),?)";
+		this.getJdbcTemplate().update(audit, new Object[]{
+				getLastAuditId(),
+				cus.getCus_id(),
+				"Customer",
+				user.getUserModel().getUsr_name(),
+				"Created row on Customer name="+cus.getCus_name()+", code="+cus.getCus_code()+", key_account="+key_acc.getKey_acc_name()
+				+", address="+cus.getAddress()+", contact_person="+cus.getContact_person()+", e-mail="+cus.getCus_email()
+				+", phone="+cus.getCus_phone()+", bill_to="+cus.getBill_to()+", payment="+cus.getPayment()+", transfer_dtl="+cus.getTransfer_dtl()
+		});
+		
 	}
 
 	@Override
 	public void deleteCustomer(int id) {
 
+		Customer cus = new Customer();
+		cus = getJdbcTemplate().queryForObject("select * from customer where cus_id ="+id, new BeanPropertyRowMapper<Customer>(Customer.class));
+		
 		String sql = "delete from customer where cus_id = "+id;
 		
 		getJdbcTemplate().update(sql);
+		
+		UserDetailsApp user = UserLoginDetail.getUser();
+		String audit = "INSERT INTO audit_logging (aud_id,parent_id,parent_object,commit_by,commit_date,commit_desc) VALUES (?,?,?,?,now(),?)";
+		this.getJdbcTemplate().update(audit, new Object[]{
+				getLastAuditId(),
+				id,
+				"Customer",
+				user.getUserModel().getUsr_name(),
+				"Deleted row on Customer name="+cus.getCus_name()+", code="+cus.getCus_code()
+		});
 		
 	}
 	
